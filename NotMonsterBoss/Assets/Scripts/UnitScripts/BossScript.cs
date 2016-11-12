2ee3e8ff97505f216773ea82483d1cd9fb9234f3
AMH AsusLaptop <abherrera1994@gmail.com>
2016-11-12 13:20:23 -0500

    Added new files

---
+++ NotMonsterBoss/Assets/Scripts/UnitScripts/BossScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossScript : UnitScript {
+
+    [SerializeField]
+    private 
+        string m_name;
+    public 
+        string boss_name { get { return m_name; } set { m_name = value; } }
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
